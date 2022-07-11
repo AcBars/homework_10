3ad234edc1c1fbe5d19300b3ff46b31020602967
Artem Mikhailob <mobail_2003@bk.ru>
2022-07-11 17:01:58 +0300

    homework_10 ver.2 add Lib

---
+++ MyLib/Ex.cs
@@ -0,0 +1,89 @@
+﻿namespace MyLib;
+using System;
+using static System.Console;
+public class Ex
+{
+    public static int [,] GetArray (int m, int n)
+    {
+        int [,] result=new int [m, n];
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                result[i,j]=new Random().Next(-100,101);
+            }
+        }
+    return result;
+    }
+
+    public static int [,] GetArray (int m, int n, int minValue, int maxValue)
+    {
+        int [,] result=new int [m, n];
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                result[i,j]=new Random().Next(minValue,maxValue);
+            }
+        }
+    return result;
+    }
+
+    public static void PrintArray (int [,] result)
+    {
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Write($"{result[i, j]} \t ");
+            }
+            WriteLine();
+        }
+    }
+
+    public static void PrintArray (double [] result)
+    {
+        for (int i = 0; i < result.Length; i++)
+        {
+
+            Write($"{result[i]} \t ");
+            WriteLine();
+        }
+    }
+
+    public static int FainMinNubers(int [,] array)
+    {
+        int result=array[0,0];
+        for(int i=0; i<array.GetLength(0); i++)
+        {
+            for(int j=0; j<array.GetLength(1); j++)
+            {
+                result=result>array[i,j]? array[i,j]: result+0;
+            }
+        }
+        return result;
+    }
+
+
+        public static int [] FainIndexMinNubers(int [,] array)
+    {
+        int [] result={array[0,0],0,0};
+        for(int i=0; i<array.GetLength(0); i++)
+        {
+            for(int j=0; j<array.GetLength(1); j++)
+            {
+                if(result>array[i,j])
+                {
+                    result[0]=array[i,j];
+                    result[i]=i;
+                    result[j]=j;
+                }
+            }
+        }
+        return result;
+    }
+
+
+}
+
+
--- task_59/Program.cs
+++ task_59/Program.cs
@@ -1,3 +1,21 @@
 ﻿/*
 Задача 59: Из двумерного массива целых чисел удалить строку и столбец, на пересечении которых расположен наименьший элемент.
 */
+
+using static System.Console;
+using MyLib;
+using System.Linq;
+
+Clear();
+WriteLine("Введите размер матрицы А через пробел: ");
+string[] sizeA = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int mA = int.Parse(sizeA[0]);
+int nA = int.Parse(sizeA[1]);
+int[,] array_A =Ex.GetArray(mA, nA);
+Ex.PrintArray(array_A);
+int [] CoordinMinNumbers=Ex.FainMinNubers(array_A);
+
+int [,] NewArray(int [,] array)
+{
+    int [,] result=
+}
