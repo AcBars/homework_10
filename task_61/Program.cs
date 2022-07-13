dbb050a83de86991b9006d3795b7aeaa37c196a8
Mikhaylov Artem <gits.info20@gmail.com>
2022-07-14 00:38:52 +0300

    homework_10 ver.7 task_61

--- task_61/Program.cs
+++ task_61/Program.cs
@@ -2,9 +2,32 @@
 Задача 61: Показать треугольник Паскаля. *Сделать вывод в виде равнобедренного треугольника.*/
 
 
-
-
 using static System.Console;
-
-
 Clear();
+Write("Введите число строк");
+int row=int.Parse(ReadLine());
+
+int [,] array(int size)
+{
+    int [,] result=new int[size+1,size+1];
+    for(int i=1; i<size; i++)
+    {
+        for(int  j=-i; j<=i; j+=2)
+        {
+            result[i,j]=result[]
+        }
+    }
+}
+
+
+for(int i=0; i<row; i++)
+{
+    SetCursorPosition(0, i);
+    Write(i+1);
+    for(int j=-i; j<=i; j+=2)
+    {
+        SetCursorPosition(WindowWidth/2-j, i);
+        Write(1);
+    }
+    
+}
